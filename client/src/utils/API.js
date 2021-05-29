a9c9f029e12c8bed501c7c518df861b122194d3e
Dora O <dorozco2390@utexas.edu>
2021-05-28 23:57:28 -0500

    updated styling

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,15 +5,18 @@ export default {
   getBook: function (query) {
     return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
   // Deletes the book with the given id
   deleteBook: function (id) {
     return axios.delete("/api/books/" + id).then(result => result.data);
   },
   // Saves a book to the database
   saveBook: function (bookData) {
-    return axios.post("/api/books", bookData).then(result => result.data);
+    return axios.post("/api/books", bookData);
   },
-  // Get the saved a books from the database
   savedBooks: function () {
     return axios.get("/api/books").then(result => result.data);
   }
