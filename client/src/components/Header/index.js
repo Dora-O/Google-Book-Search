cc5274736f6b77b86be10145ee6a7a66741895dd
Dora O <dorozco2390@utexas.edu>
2021-05-28 23:56:32 -0500

    added styling

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Header() {
     return (
-        <div className="jumbotron text-center">
-            <div className="container text-light">
+        <div className="jumbotron text-center" style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}>
+            <div className="container text-dark">
                 <h1>Google Books Search</h1>
+                <h2>Search for and save Books of Interest</h2>
             </div>
         </div>
     );
